266cd9b8006fc9d9aa744d6b2155cbe8bb293254
Iverson <muhammad.ikhwandd@gmail.com>
2025-04-22 15:10:56 +0800

    Add new file

--- main.tf
+++ main.tf
@@ -1,2 +1,24 @@
+terraform {
+  required_providers {
+    aws={
+      source = "hashicorp/aws"
+      version= "4.0"
+    }
+  }
+}
+
 provider "aws" {
-  region     = "ap-southeast-1"}
+  region = "ap-southeast-1b"
+}
+
+## Create new EC2 Instance
+resource ""aws_instance"" "HiEC2" {
+  ami               = "ami-01938df366ac2d954"
+  instance_type     = "t2.micro"
+
+  tags = {
+    Name = "EC2Instance"
+  }
+}
+
+# Output public IP
